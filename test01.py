28ca1347316b1c43208ad1102b3b9663c314db50
EngineNext <43810611+EngineNext@users.noreply.github.com>
2022-03-15 21:05:41 +0900

    hello

--- test01.py
+++ test01.py
@@ -3,3 +3,8 @@ from PIL import Image
 import numpy as np
 
 st.file_uploader("Upload an image", type=["jpg", "png"])
+
+uploaded_file=st.file_uploader("ファイルアップロード", type='png')
+image=Image.open(uploaded_file)
+img_array = np.array(image)
+st.image(img_array,caption = 'サムネイル画像',use_column_width = True)
